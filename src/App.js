b36a7d926f7860c3aac76de4765e3476f433468e
MANOUAN CEDRIC PASCAL-EMMANUEL <manouancedric@gmail.com>
2020-12-01 14:39:37 +0000

    first commit v1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          React is so cool ... let's get started
+        </p>
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
